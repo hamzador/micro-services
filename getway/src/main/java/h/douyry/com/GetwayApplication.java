eacb5b165775e286a8a87aea622c70da0f523159
hamzador <hamozadouyry@gmail.com>
2025-07-14 23:59:03 +0200

    routage dynamique

--- getway/src/main/java/h/douyry/com/GetwayApplication.java
+++ getway/src/main/java/h/douyry/com/GetwayApplication.java
@@ -2,12 +2,21 @@ package h.douyry.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GetwayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GetwayApplication.class, args);
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient reactiveDiscoveryClient, DiscoveryLocatorProperties discoveryLocatorProperties){
+		return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient, discoveryLocatorProperties);
 	}
 
 }
