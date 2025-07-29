f53efad19442a8468072ea1cee81105a4d20ea44
hamzador <hamozadouyry@gmail.com>
2025-07-30 00:06:01 +0200

    mouve the config to config repo

--- billing-service/src/main/java/h/douyry/com/billing_service/feign/ProductRestClient.java
+++ billing-service/src/main/java/h/douyry/com/billing_service/feign/ProductRestClient.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "inventory-service" )
 public interface ProductRestClient {
 
-    @GetMapping("/api/products/{id}")
+    @GetMapping("/products/{id}")
     Product getProductById(@PathVariable String id);
 
 
-    @GetMapping("/api/products")
+    @GetMapping("/products")
     PagedModel<Product> getAllProduct();
 }
